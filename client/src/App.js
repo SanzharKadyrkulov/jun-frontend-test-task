2a4328d4db7ba2629c26131cb6241bc2b01dc117
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2022-08-10 11:31:27 +0600

    installed socked.io-client, got data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
 import './App.css';
 
+const socket = io('http://localhost:3002');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		socket.emit('start');
+
+		socket.on('ticker', (data) => {
+			console.log(data);
+		});
+	}, []);
+	return <div>Hello World</div>;
 }
 
 export default App;
